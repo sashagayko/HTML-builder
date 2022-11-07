a37817309ccf205afe6ce03ec309136e7ebc073d
Sasha <hlupik69@gmail.com>
2022-11-08 02:33:21 +0300

    add solution 01

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+const fs = require('fs');
+const stream = fs.createReadStream(filePath, 'utf-8');
+stream.on('data', chunk => {
+    console.log(chunk.toString())
+});
