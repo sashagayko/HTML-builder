0ec6e39df77fff166071f0f35cf0fe13cd04529e
Sasha <hlupik69@gmail.com>
2022-11-09 02:54:32 +0300

    fix solution 01

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,6 +2,4 @@ const path = require('path');
 const filePath = path.join(__dirname, 'text.txt');
 const fs = require('fs');
 const stream = fs.createReadStream(filePath, 'utf-8');
-stream.on('data', chunk => {
-    console.log(chunk.toString())
-});
+stream.on('data', data => console.log(data));
