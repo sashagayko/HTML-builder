bd375980caf726cd0bdad5183df535f025bffe86
Sasha <hlupik69@gmail.com>
2022-11-10 17:01:09 +0300

    add solution 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
-
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'files-copy');
+fs.mkdir(filePath, { recursive: true }, (err)=> {
+    if (err) {
+        console.log(err);
+    }
+})
+fs.readdir(path.join(__dirname, 'files'), (err, files)=> {
+    if (err) {
+        console.log(err);
+    } else {
+        files.forEach(el => {
+            fs.copyFile(path.join(__dirname,'files', `${el}`), path.join(__dirname,'files-copy', `${el}`), err=> {
+                if (err) {
+                    console.log(err);
+                }
+            })
+        });
+    }
+})
